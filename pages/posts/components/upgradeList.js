8624b263814500cbc84bbe40f9343445157df85f
CEODemitri <126187234+CEODemitri@users.noreply.github.com>
2023-06-27 23:28:23 -0500

    Is this Working

--- pages/posts/components/upgradeList.js
+++ pages/posts/components/upgradeList.js
@@ -2,7 +2,9 @@ function upgradeList() {
     return (
         <>
             <div className="bg-color-black-400">
-
+                <h1>
+                    <p>List Of Scheduled Upgrdes</p>
+                </h1>
             </div>
         </>
     );
